579fb19d0966e0ac1e069f9d75e9975631de28c5
AyameEmilia <kafune.ch@gmail.com>
2024-06-14 21:14:46 +0700

    chore: manager

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,5 @@ module.exports = (fastify) => {
     fastify.register(require(path.join(__dirname, 'api/' + "auth.js")), { prefix: 'api//' + "auth" });
     fastify.register(require(path.join(__dirname, 'api/' + "avatar.js")), { prefix: 'api' });
     fastify.register(require(path.join(__dirname, 'api/' + "general.js")), { prefix: 'api' });
+    fastify.register(require(path.join(__dirname, 'api/' + "manager.js")), { prefix: 'manager' });
 }
