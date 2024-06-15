c132521837bff187c3f46244d2570b6a79eb4bdf
AyameEmilia <kafune.ch@gmail.com>
2024-06-15 14:04:00 +0700

    change: key logic

--- routes/api/manager.js
+++ routes/api/manager.js
@@ -3,7 +3,7 @@ const cache = require("../../cache")
 module.exports = (fastify, opts, done) => {
 
     fastify.get("/serverdata", async (req, res) => {
-        if (req.headers.magicadmin != "do not ddos me!") return {}
+        if (req.headers.key != process.env.THIS_IS_PASSWORD) return res.code(403).send("Do not have permission to do this.")
         let ss = cache.sessions.map(e => {
             return {
                 owner: e.owner,
